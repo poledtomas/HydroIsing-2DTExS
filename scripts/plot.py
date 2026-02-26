60618515bd9d20b152a67e0988106b50a1c1a1eb
Tomáš Poledníček <polednicek.tomas@seznam.cz>
2026-02-26 12:13:25 +0100

    add plotting script

---
+++ scripts/plot.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+dat = np.loadtxt("../invIsing-2DTExS.dat")
+
+NTt, Nmbt = 500, 500
+dat = dat.reshape(NTt, Nmbt, -1)
+
+fig, ax = plt.subplots(1, 1, figsize=(9, 8))
+for i in range(Nmbt):
+    arr = dat[:, i]
+    T, e = arr[:, 4], arr[:, 2]
+    ax.plot(T, e / T**4, lw=1)
+ax.set_xlabel("$T$ (GeV)")
+ax.set_ylabel("$\\epsilon/T^4$")
+ax.grid(True)
+plt.savefig("Teps.png", format="png", dpi=300)
+plt.show()
+
+
+fig, ax = plt.subplots(1, 1, figsize=(9, 8))
+for i in range(NTt - 340):  # To see the 1st order line under the folding.
+    arr = dat[i, :]
+    mub, nb, T = arr[:, 5], arr[:, 3], arr[:, 4]
+    ax.plot(nb / T**3, mub, lw=1)
+
+ax.set_xlabel("$n_B/T^3$")
+ax.set_ylabel("$\\mu_B$ (GeV)")
+ax.set_xlim(-0.05, 1.64)
+# ax.set_xlim(0.41, 0.66)
+# ax.set_ylim(0.4, 0.54)
+# ax.grid(True)
+plt.savefig("mubnb.png", format="png", dpi=300)
+plt.show()
+
+
+fig, ax = plt.subplots(1, 1, figsize=(9, 8))
+for i in range(100, NTt - 100):
+    arr = dat[i, :]
+    chi2, nb, T = arr[:, 9], arr[:, 3], arr[:, 4]
+    ax.plot(nb / T**3, chi2 / T**2, lw=1)
+
+ax.set_xlabel("$n_B/T^3$")
+ax.set_ylabel("$\\chi_2/T^2$")
+ax.set_xlim(-0.05, 1.37)
+# ax.set_xlim(0.41, 0.66)
+ax.set_ylim(-0.01, 0.9)
+ax.grid(True)
+plt.savefig("chinb.png", format="png", dpi=300)
+plt.show()
